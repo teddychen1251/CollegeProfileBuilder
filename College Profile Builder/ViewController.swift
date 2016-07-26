b0c29acc38d1f63137b58425bd9a65d725ca821b
SESP Walkup <sespwalkup@staffmac08-sesp.local>
2016-07-26 14:41:58 -0500

    Allow for entries to be added

--- College Profile Builder/ViewController.swift
+++ College Profile Builder/ViewController.swift
@@ -35,4 +35,39 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             tableView.reloadData()
         }
     }
+    
+    @IBAction func onTappedPlusButton(sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Add college here"
+        }
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) in
+            let collegeTextField = alert.textFields![0] as UITextField
+            self.colleges.append(College(name: collegeTextField.text!))
+            self.tableView.reloadData()
+        }
+        alert.addAction(addAction)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancelAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
